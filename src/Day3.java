89f529fe2d3ea53555a62e98255cae9bfc2410d1
PrakharGupta016 <86834271+PrakharGupta016@users.noreply.github.com>
2024-08-13 00:46:25 +0530

    day 3 part 2 with custom pair

--- src/Day3.java
+++ src/Day3.java
@@ -1,12 +1,112 @@
-import javax.rmi.ssl.SslRMIClientSocketFactory;
 import java.io.BufferedReader;
 import java.io.FileReader;
-import java.util.ArrayList;
+import java.util.*;
+
+ class Pair<K, V> {
+    private final K key;
+    private final V value;
+
+    public Pair(K key, V value) {
+        this.key = key;
+        this.value = value;
+    }
+
+    public K getKey() {
+        return key;
+    }
+
+    public V getValue() {
+        return value;
+    }
+
+    @Override
+    public String toString() {
+        return "(" + key + ", " + value + ")";
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Pair<?, ?> pair = (Pair<?, ?>) o;
+        return key.equals(pair.key) && value.equals(pair.value);
+    }
+
+    @Override
+    public int hashCode() {
+        return key.hashCode() * 31 + value.hashCode();
+    }
+}
 
 public class Day3 {
     private static int part2(ArrayList<String>arr)
     {
-       return 0;
+        int ans = 0 ;
+        int temp = 0 ;
+
+        int n = arr.size();
+        int m = arr.get(0).length();
+        HashMap<Pair,ArrayList<Integer>> hashMap = new HashMap<>();
+        for(int i = 0 ; i  < n ;  i++)
+        {
+            HashSet<Pair> possibleLocations = new HashSet<Pair>();
+            for(int j = 0 ; j<m; j++)
+            {
+                char ch = arr.get(i).charAt(j);
+                if(Character.isDigit(ch))
+                {
+                    temp  = temp *10 + ((ch) - '0');
+                    for(int x = 0; x<directions.length ; x++)
+                    {
+                        int newX = directions[x][0]+i;
+                        int newY =  directions[x][1]+j;
+                        if(newX>=0 && newX<n && newY>=0 && newY<m && arr.get(newX).charAt(newY) == '*')
+                        {
+                            possibleLocations.add(new Pair(newX,newY));
+                        }
+                    }
+                }
+                else
+                {
+                    if(temp>0)
+                    {
+                        for(Pair pair : possibleLocations)
+                        {
+                            ArrayList<Integer> ar = hashMap.getOrDefault(pair,new ArrayList<>());
+                            ar.add(temp);
+                            hashMap.put(pair,ar);
+                        }
+                    }
+                    temp = 0;
+                    possibleLocations.clear();
+                }
+            }
+
+            if(temp>0)
+            {
+                for(Pair pair : possibleLocations)
+                {
+                    ArrayList<Integer> ar =  hashMap.getOrDefault(pair,new ArrayList<>());
+                    ar.add(temp);
+                    hashMap.put(pair,ar);
+                }
+            }
+            temp = 0;
+            possibleLocations.clear();
+        }
+        for(Map.Entry<Pair,ArrayList<Integer>> entries : hashMap.entrySet())
+        {
+            if(entries.getValue().size() == 2)
+            {
+                int temp1 = 1;
+                for(Integer val:entries.getValue())
+                {
+                    temp1*=val;
+                }
+                ans+=temp1;
+            }
+        }
+        return ans;
     }
     private static boolean validCharacter(char ch)
     {
@@ -80,6 +180,6 @@ public class Day3 {
         }
         int ans = part1(arr);
         int ans2 = part2(arr);
-        System.out.println(ans);
+        System.out.println(ans2);
     }
 }
